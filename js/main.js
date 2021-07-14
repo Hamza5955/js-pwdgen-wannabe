5f0bb054fa451bcf60b45c36b1d4c6319b4122ef
Hamza <Hamza5955>
2021-07-15 01:14:01 +0200

    bonus

--- js/main.js
+++ js/main.js
@@ -2,12 +2,15 @@
 poi chiedi il suo cognome,
 poi chiedi il suo colore preferito
 Infine scrivi sulla pagina nomecognomecolorepreferito21*/
-var Name = prompt("Dimmi il tuo nome?");
 
-var cognome = prompt("Dimmi il tuo cognome?");
+var message = alert("Crea il tuo nome perfetto per wallstreetbets");
 
-var Color = prompt("Dimmi il tuo colore preferito?");
+var Name = prompt("Dimmi il tuo nome,?", "degenerates");
 
-var Year = 21;
+var Cognome = prompt("Dimmi il tuo cognome?", "idiot");
 
-document.getElementById("password").innerHTML = Name + cognome + Color + Year;
+var Color = prompt("Dimmi il tuo colore preferito?", "black");
+
+document.getElementById("password").innerHTML = Name + Cognome + Color + "21";
+
+document.getElementById("bottom").innerHTML = "YOLO you only live once";
